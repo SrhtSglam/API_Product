fbefd2b3f46665864f490bcf3863d79fd70a2401
Serhat Sağlam <serhatsaglamb@gmail.com>
2024-10-23 14:18:47 +0300

    Build002

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -6,24 +6,30 @@ namespace ProductAPI.Controllers{
     [ApiController]
     [Route("[controller]")]
     public class ProductController : ControllerBase{
+        List<Product> ProductList;
+
         [HttpGet]
         public IEnumerable<Product> GetAll(){
-            return Enumerable.Range(1, 3).Select(index => new Product
-            {
-                Id = 0,
-
-            }).ToArray();
+            query();
+            return ProductList;
         }
 
-        // [HttpPost]
-        // public List<Product> AddProduct(){
-        //     return Enumerable<Product>new Product{
-        //         Id = 0,
-        //         CategoryId = 1,
-        //         Name = "Serhat",
-        //         Description = "depwalfpğwelaflpğeflpğa"
-                
-        //     }).ToArray();
-        // }
+        [HttpPost]
+        public IEnumerable<Product> AddProduct(){
+            query();
+            ProductList.Add(new Product(){
+                Id = 0,
+                CategoryId = 1,
+                Name = "Akıllı Saat",
+                Description = "Bu bir saat"
+            });
+            return ProductList;
+        }
+
+        private void query(){
+            if(ProductList == null){
+                ProductList = new List<Product>();
+            }
+        }
     }
 }
--- ProductAPI/Product.cs
+++ ProductAPI/Product.cs
@@ -3,7 +3,7 @@ namespace ProductAPI{
     {
         public int Id { get; set; }
         public int CategoryId { get; set; }
-        public int Name { get; set; }
-        public int Description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
